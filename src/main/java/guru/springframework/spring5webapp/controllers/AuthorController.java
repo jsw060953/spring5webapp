0dac66c42391cbeaa756a02ac72345e1db78d5be
John Stanford White <john.stanford.white@gmail.com>
2023-05-01 12:04:37 -0400

    Working with Thymeleaf to show books

--- src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -6,10 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BookController {
+public class AuthorController {
     private  BookRepository bookRepository;
 
-    public BookController(BookRepository bookRepository) {
+    public AuthorController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
